86c1930512dd0a0f340830e2fd3fbf981c56f191
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2025-10-01 12:08:16 +0200

    Comments component shows "Add comment" button when user is logged in

--- src/parts/Comments.tsx
+++ src/parts/Comments.tsx
@@ -1,24 +1,33 @@
+import { Button } from "react-bootstrap";
 import { useApi } from "../hooks/useApi";
 import type Comment from "../interfaces/Comment"
 import CommentCard from "./CommentCard";
+import { useAuth } from "../contexts/AuthContext";
 interface CommentsProps {
   noticeId: number
 }
 
 export default function Comments({ noticeId }: CommentsProps) {
   const { data: comments, loading, error } = useApi<Comment[]>(`/api/comments?where=noticeId=${noticeId}`)
-
+  const { isLoggedIn, user } = useAuth()
   if (loading) return <div>loading comments</div>
   if (error) return <div>Error: {error}</div>
 
   if (comments?.length === 0) return <span>0 comments</span>
   if (comments?.length === 1) console.log('kommmentarer: ', comments)
 
+
   return (<>
-    {comments?.map((comment) => {
-      return (
-        <CommentCard comment={comment} />
-      )
-    })}
+    {isLoggedIn && <div><Button variant="outline-primary" size="sm" className="btn-small">Add comment</Button></div>
+
+    }
+
+    {comments?.length === 0 ? <div>0 comments</div> :
+      comments?.map((comment) => {
+        return (
+          <CommentCard comment={comment} />
+        )
+      })
+    }
   </>)
 }
