f36177a3dae627a5287e5a5aae5ba77ddc6c8982
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2025-10-02 11:26:54 +0200

    Users can now add comments to notices with a modal.

--- src/parts/Comments.tsx
+++ src/parts/Comments.tsx
@@ -2,25 +2,42 @@ import { Button } from "react-bootstrap";
 import { useApi } from "../hooks/useApi";
 import type Comment from "../interfaces/Comment"
 import CommentCard from "./CommentCard";
+import CreateCommentModal from "./CreateCommentModal";
 import { useAuth } from "../contexts/AuthContext";
+import { useState } from "react";
 interface CommentsProps {
   noticeId: number
 }
 
 export default function Comments({ noticeId }: CommentsProps) {
-  const { data: comments, loading, error } = useApi<Comment[]>(`/api/comments?where=noticeId=${noticeId}`)
-  const { isLoggedIn, user } = useAuth()
+  const { data: comments, loading, error, refetch } = useApi<Comment[]>(`/api/comments?where=noticeId=${noticeId}`)
+  const { isLoggedIn } = useAuth()
+  const [showCreateCommentModal, setShowCreateCommentModal] = useState(false)
+
+
   if (loading) return <div>loading comments</div>
   if (error) return <div>Error: {error}</div>
-
-  if (comments?.length === 0) return <span>0 comments</span>
   if (comments?.length === 1) console.log('kommmentarer: ', comments)
 
 
+  function handleClick() {
+    setShowCreateCommentModal(true)
+
+  }
+
   return (<>
-    {isLoggedIn && <div><Button variant="outline-primary" size="sm" className="btn-small">Add comment</Button></div>
+    {isLoggedIn && <div>
+      <Button
+        onClick={handleClick}
+        variant="outline-primary"
+        size="sm"
+        className="btn-small">
+        Add comment
+      </Button>
+    </div>
 
     }
+
 
     {comments?.length === 0 ? <div>0 comments</div> :
       comments?.map((comment) => {
@@ -29,5 +46,14 @@ export default function Comments({ noticeId }: CommentsProps) {
         )
       })
     }
+
+    <CreateCommentModal
+      noticeId={noticeId}
+      show={showCreateCommentModal}
+      onHide={() => setShowCreateCommentModal(false)
+      }
+      onSuccess={refetch}
+
+    />
   </>)
 }
